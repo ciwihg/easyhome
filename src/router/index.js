581d299078e1c6d6229364e2565e80d202dc8b3d
ciwi <378361812@qq.com>
2017-05-18 15:27:17 +0800

    install muse-ui

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+
+Vue.use(MuseUI)
 
 Vue.use(Router)
 
