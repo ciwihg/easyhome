96cf622453842c49207e092ac4419010adb47c31
ciwi <378361812@qq.com>
2017-06-02 17:59:05 +0800

    save friday work

--- src/components/mobileslider.vue
+++ src/components/mobileslider.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div class="item_wrap">
+  <div @touchstart="ts" @touchmove="tm" @touchend="te">
+    <div class="item_wrap" ref="itemwrap">
     <div class="item">
       <a href="#">
         <img src="static/t1.jpg"/>
@@ -26,12 +26,53 @@
 </template>
 
 <script>
+export default{
+  name:"mobileslider",
+  data:function(){
+    return {
+      tspos:0,
+      tdistance:0,
+      tactive:33
+    }
+  },
+  methods:{
+    ts:function(e){
+      if(e.targetTouches.length==1){
+        var touch=e.targetTouches[0];
+        this.tspos=touch.pageX;
+        this.tdistance=0;
+        console.log(touch.pageX);
+      }
+    },
+    tm:function(e){
+      if(e.targetTouches.length==1){
+        var touch=e.targetTouches[0];
+        this.tdistance=this.tspos-touch.pageX;
+        if(this.tactive==33&&this.tdistance<0){
+        }
+        else
+        {this.$refs.itemwrap.style.left=this.tactive-this.tdistance+"px";}
+      }
+    },
+    te:function(e){
+      if(this.tdistance>0){
+        (Math.abs(this.tdistance)>=100)?(this.$refs.itemwrap.style.left=this.tactive-346+'px',this.tactive=this.tactive-346):(this.$refs.itemwrap.style.left=this.tactive+'px')
+      }
+      else if(this.tdistance<0){
+        (Math.abs(this.tdistance)>=100&&this.tactive!=33)?(this.$refs.itemwrap.style.left=this.tactive+346+'px',this.tactive=this.tactive+346):(this.$refs.itemwrap.style.left=this.tactive+'px')
+      }
+    }
+  }
+}
 </script>
 
 <style scoped>
 .item_wrap{
   width:1000%;
   overflow: hidden;
+  left:0px;
+  position: relative;
+  transition: left 400ms;
 }
 .item_wrap::after{
   content: "";
@@ -45,11 +86,12 @@
    margin-right:0.2%;
 }
 .item img{
-  max-width: 100%;
+  width: 100%;
 }
 .item>a{
   display: inline-block;
   position: relative;
+  width: 100%;
 }
 .mask{
    width:100%;
